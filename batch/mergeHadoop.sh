3180948aae158f0ebe7166b4a49a2cb8949a7f9d
gzevi <giovanni.zevidellaporta@gmail.com>
2016-05-10 18:45:39 -0700

    TChain based merging. Currently a separate step not integrated with scripts. This is how it is done in MT2

---
+++ batch/mergeHadoop.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+function run () {
+    echo root -b -q mergeHadoopFiles.C\(\"${HADOOPDIR}/$1\",\"${OUTPUTDIR}/$1.root\"\)
+    nohup nice -n 19 root -b -q mergeHadoopFiles.C\(\"${HADOOPDIR}/$1\",\"${OUTPUTDIR}/$1.root\"\) >& log_merge_$1.txt &
+}
+
+#TAG=V00-01-09_25ns_fastsim
+
+HADOOPDIR=/hadoop/cms/store/user/${USER}/condor/forCommissioning/
+#HADOOPDIR=/hadoop/cms/store/user/${USER}/condor/forSoftLeptons74X_1May16/
+OUTPUTDIR=/nfs-7/userdata/leptonTree/v1.07TP/
+
+mkdir -p $OUTPUTDIR
+chmod -R a+wrx $OUTPUTDIR
+
+
+#run 2015DSingleEl76X
+#run DY_76X_HCALdebug
+run 2015DSingleElV4
+run 2015DSingleMuonV4
+run DY_madgraph
